0b86666ac7c213bfaf13265a9f0c465308019852
anujsharma <sharma.anuj15.github@gmail.com>
2022-02-28 19:20:23 -0500

    Made change in bootstrap of home

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -8,7 +8,7 @@ import PostSummaryList from "../PostSummaryList/PostSummaryList.js";
         <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-2 col-xxl-2 mt-2 wd-border-right">
                 ${NavigationSidebar("home")}
             </div>
-            <div class="col-11 col-sm-11 col-md-11 col-lg-6 col-xl-6 col-xxl-6 mt-2 wd-border-right">
+            <div class="col-11 col-sm-11 col-md-11 col-lg-7 col-xl-6 col-xxl-6 mt-2 wd-border-right">
                 ${PostItems()}
             </div>
             <div class="d-none d-lg-block col-lg-4 col-xl-4 col-xxl-4 mt-2">
