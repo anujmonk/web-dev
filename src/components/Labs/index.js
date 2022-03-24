24d2b4dd30e520fa816c178ae112743b63d2f343
anujsharma <sharma.anuj15.github@gmail.com>
2022-03-23 23:07:43 -0400

    Completed Lab A7

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -4,12 +4,14 @@ import Todo from './Todo';
 import Classes from './Classes';
 import ConditionalOutput from './ConditionalOutput';
 import Styles from './Styles';
+import ReduxExamples from './redux-examples';
 
 const Labs = () => {
     return(
         <span className="text-white">
         <h1>Labs</h1>
         <Link to="/hello">Hello</Link> | <Link to="/tuiter">Tuiter</Link>
+        <ReduxExamples />
         <Todo/>
         <ConditionalOutput/>
         <Styles/>
